d5727e23c561b0ded3e30bb8aaad6aa40dbc9ede
irakli <irakliqiria8@gmail.com>
2022-07-03 00:02:44 +0400

    add first step style

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 import validator from "./formValidate.js";
 
-
 const inputValue = document.querySelectorAll(".inputValue")
 // error array
 const errors = []
+// step
+const step = document.querySelector(".first_step .number")
 
 // submit button
 const submit = document.getElementById("submit");
@@ -17,18 +18,24 @@ submit.addEventListener("submit", ()=> {
     const date = document.getElementById("date");
 
     validator(name, email, phone, date, errors)
+    console.log(errors);
 })
+
+
 
 // save input value in localStorage
 inputValue.forEach(item => {
-    item.addEventListener("input", (e) => {
+    item.addEventListener("input", () => {
         localStorage.setItem(item.name, item.value);
-    })    
-});
-
-
-inputValue.forEach(item => { 
+        
+        // change step style
+        if (item.value.length > 0){
+            step.classList.add("active")
+        }
+    })
     if (localStorage.getItem(item.name)){
+        step.classList.add("active")
         item.value =localStorage.getItem(item.name)
     }
 });
+
