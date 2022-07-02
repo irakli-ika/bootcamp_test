a11e795bbedfb1f95d8dcf5e3002f129fbb0e8c3
irakli <irakliqiria8@gmail.com>
2022-07-02 23:32:43 +0400

    completed form validation, add localStorage

--- js/formValidate.js
+++ js/formValidate.js
@@ -1,17 +1,21 @@
-const validator = (name, email, phone) => {
+const validator = (name, email, phone, date, errors) => {
         // check name
     if (name.value.length == 0 || name.value.length < 2) {
-        console.log("error");
+        errors.push({"name": "Please enter valid name"})
     } else console.log("ok");
     // check email
     if (email.value.length == 0 || !email.value.includes("@redberry.ge")) {
-        console.log("email error");
+        errors.push({"name": "Please enter valid email address"})
     } else console.log("ok");
     // check phone
     if (phone.value.length == 0 || !(phone.value.length == 9)
         || !(phone.value.match(/[0-9]/g)) || phone.value.match(/[a-zA-Z]/g)) {
-        console.log("error");
-    } else console.log("opk");
+            errors.push({"name": "Please enter valid phone"})
+    } else console.log("ok");
+    // check date
+    if (date.value == "") {
+        errors.push({"name": "Please enter date of birth"})
+    } else console.log("ok");
 }
 
 export default validator
--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
 import validator from "./formValidate.js";
 
-// inputs
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const date = document.getElementById("date");
+
+const inputValue = document.querySelectorAll(".inputValue")
+// error array
+const errors = []
+
 // submit button
 const submit = document.getElementById("submit");
 
+// submit form
 submit.addEventListener("submit", ()=> {
-    validator(name, email, phone)
+    // inputs
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
+    const date = document.getElementById("date");
+
+    validator(name, email, phone, date, errors)
 })
+
+// save input value in localStorage
+inputValue.forEach(item => {
+    item.addEventListener("input", (e) => {
+        localStorage.setItem(item.name, item.value);
+    })    
+});
+
+
+inputValue.forEach(item => { 
+    if (localStorage.getItem(item.name)){
+        item.value =localStorage.getItem(item.name)
+    }
+});
